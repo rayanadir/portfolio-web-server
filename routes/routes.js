b343a11fb0726c635dfa9fb8064c04ce2301ea9e
rayanadir <rayan.dahmena@gmail.com>
2024-01-19 19:10:21 +0100

    add admin middleware

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const conversationController = require('../controllers/conversation.controller')
 const messageController = require('../controllers/message.controller')
 const auth = require("../middleware/auth.middleware")
 const resetMiddleware = require("../middleware/reset.middleware")
+const admin = require("../middleware/admin.middleware.js");
 
 // register
 router.post("/api/register", authController.signUp);
@@ -16,13 +17,13 @@ router.post("/api/login", authController.signIn);
 router.post("/api/logout", authController.logout);
 
 // loggedIn
-router.get("/api/loggedIn", authController.loggedIn);
+router.get("/api/loggedIn", auth,authController.loggedIn);
 
 // resetPassword
 router.put("/api/resetPassword/:token", resetMiddleware, authController.resetPassword);
 
 // forgotPassword
-router.put("/api/forgotPassword", authController.forgotPassword);
+router.put("/api/forgotPassword", auth,authController.forgotPassword);
 
 // changePassword
 router.put("/api/changePassword/:token", auth ,authController.changePassword);
@@ -34,36 +35,36 @@ router.post("/api/getUser", auth,userController.getUser);
 router.post("/api/checkToken", resetMiddleware,authController.checkToken)
 
 // get admin username
-router.post("/api/adminUsername", conversationController.getAdmin);
+router.post("/api/adminUsername", auth,conversationController.getAdmin);
 
 // start new conversation
-router.post("/api/newConversation",conversationController.newConversation);
+router.post("/api/newConversation", auth,conversationController.newConversation);
 
 // send message
-router.post("/api/sendMessage", conversationController.sendMessage);
+router.post("/api/sendMessage", auth,conversationController.sendMessage);
 
 // get single conversation
-router.post("/api/conversation/:id", conversationController.getSingleConversation);
+router.post("/api/conversation/:id", auth,conversationController.getSingleConversation);
 
 // get all conversations
-router.post("/api/conversations",conversationController.getAllConversations);
+router.post("/api/conversations", auth,conversationController.getAllConversations);
 
 // has already started a conversation ?
-router.post("/api/hasConversation", conversationController.hasConversation);
+router.post("/api/hasConversation", auth, conversationController.hasConversation);
 
 // check is valid conversation
-router.post("/api/checkConversation", conversationController.checkIsValidConversation)
+router.post("/api/checkConversation", auth,conversationController.checkIsValidConversation)
 
 // send simple message
 router.post("/api/simpleMessage", messageController.sendSimpleMessage)
 
 // get all simple messages
-router.get("/api/messages", messageController.getAllSimpleMessages)
+router.get("/api/messages", admin, messageController.getAllSimpleMessages)
 
 // get simple message
-router.post("/api/message", messageController.getSimpleMessage)
+router.post("/api/message", admin, messageController.getSimpleMessage)
 
 // check if simple message is valid
-router.post("/api/checkMessage", messageController.checkIsValidMessage)
+router.post("/api/checkMessage", auth,messageController.checkIsValidMessage)
 
 module.exports = router;
